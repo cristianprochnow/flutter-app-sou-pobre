161a0993e390f9665da303e55a4d4d2ef5b6bff5
Cristian Prochnow <cristianprochnow@gmail.com>
2023-08-18 23:47:46 -0300

    feat: add action button

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   Color? backgroundGrey = Colors.blueGrey[900];
+  Color? backgroundAccent = Colors.white;
 
   runApp(MaterialApp(
     home: Scaffold(
@@ -17,6 +18,39 @@ void main() {
           image: AssetImage('assets/images/diamond.png'),
         ),
       ),
+      floatingActionButton: AlertButton(
+        backgroundColor: backgroundAccent,
+        foregroundColor: backgroundGrey,
+      )
     ),
   ));
+}
+
+class AlertButton extends StatelessWidget {
+  Color? backgroundColor;
+  Color? foregroundColor;
+
+  AlertButton({ super.key, this.backgroundColor, this.foregroundColor });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => showDialog(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text("Tamo aí na atividade"),
+          content: const Text("Dias de luta, dias de glória!"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text("Show!"),
+            )
+          ],
+        ),
+      ),
+      backgroundColor: backgroundColor,
+      foregroundColor: foregroundColor,
+      child: const Icon(Icons.diamond),
+    );
+  }
 }
